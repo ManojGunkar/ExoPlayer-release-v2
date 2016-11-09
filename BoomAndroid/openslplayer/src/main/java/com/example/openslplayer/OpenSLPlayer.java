5939e9bc09756a805f9b3ea3242818ab389667a8
Adarsh <adarsh@globaldelight.com>
2016-11-09 16:33:50 +0530

    Decoder thread need not run at the high priority.

--- BoomAndroid/openslplayer/src/main/java/com/example/openslplayer/OpenSLPlayer.java
+++ BoomAndroid/openslplayer/src/main/java/com/example/openslplayer/OpenSLPlayer.java
@@ -176,7 +176,6 @@ public class OpenSLPlayer implements Runnable {
 
     @Override
     public void run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
             try{
                 Thread.sleep(100);
             }catch (InterruptedException e){
