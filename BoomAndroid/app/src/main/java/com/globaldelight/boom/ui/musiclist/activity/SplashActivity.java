c5e671efe8e507a3f5a0ddf7b59e932d132fc841
nidhin.ek <nidhin.ek@robosoftin.com>
2016-12-02 11:56:45 +0530

    splash screen status bar

--- BoomAndroid/app/src/main/java/com/globaldelight/boom/ui/musiclist/activity/SplashActivity.java
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/ui/musiclist/activity/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.globaldelight.boom.ui.musiclist.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.globaldelight.boom.R;
@@ -43,7 +45,12 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         if(!isExpire("DEC-30-2016")) {
             startService(new Intent(this, PlayerService.class));
             new Handler().postDelayed(new Runnable() {
