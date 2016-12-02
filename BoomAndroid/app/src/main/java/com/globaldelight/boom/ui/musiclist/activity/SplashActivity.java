286c2d12cb2d51673b8adf7e81af2e288c1f70fd
nidhin.ek <nidhin.ek@robosoftin.com>
2016-12-02 10:06:06 +0530

    Splash screen code clean

--- BoomAndroid/app/src/main/java/com/globaldelight/boom/ui/musiclist/activity/SplashActivity.java
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/ui/musiclist/activity/SplashActivity.java
@@ -1,12 +1,10 @@
 package com.globaldelight.boom.ui.musiclist.activity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Toast;
 
 import com.globaldelight.boom.R;
@@ -46,11 +44,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splash);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
         if(!isExpire("DEC-30-2016")) {
             startService(new Intent(this, PlayerService.class));
             new Handler().postDelayed(new Runnable() {
