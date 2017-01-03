4dc8dd465df289c8a83e4b6d0375f686963ee161
Adarsh <adarsh@globaldelight.com>
2017-01-03 12:27:28 +0530

    Fixed deployment errors

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-if [ -d "$DEPLOY_DIR" ]; then
-	mkdir -p "${DEPLOY_SERVER_PATH}/History"
-	mv "${DEPLOY_DIR}/Build_* ${DEPLOY_SERVER_PATH}/History/" 
-else
+# if the deployment directory does not exist create it
+if [ ! -d "$DEPLOY_DIR" ]; then
 	mkdir -p "$DEPLOY_DIR"
 fi
+
+# if the firectory contains some files then move them to history folder
+if [ "$(ls $DEPLOY_DIR)" ]; then
+	if [ ! -d "${DEPLOY_SERVER_PATH}/History" ]; then
+		mkdir -p "${DEPLOY_SERVER_PATH}/History"
+	fi
+
+	mv "${DEPLOY_DIR}"/Build* "${DEPLOY_SERVER_PATH}/History" 
+fi
+
+	
