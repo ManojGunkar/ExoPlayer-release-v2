0309e895d8b5695859f17df9c87de480316a3089
Adarsh <adarsh@globaldelight.com>
2017-08-24 12:11:38 +0530

    Optimized memory utilised by Blurring logic. Sometimes it leads to crash on devices with less RAM.

--- BoomAndroid/app/src/main/java/com/globaldelight/boom/utils/PlayerUtils.java
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/utils/PlayerUtils.java
@@ -108,14 +108,18 @@ public class PlayerUtils {
 
         scaledBitmap.recycle();
 
-        return PlayerUtils.blur(context, tempBitmap);
+        Bitmap out = PlayerUtils.blur(context, tempBitmap);
+        tempBitmap.recycle();
 
+        System.gc();
+
+        return out;
     }
 
     public static Bitmap blur(Context activity, Bitmap inputBitmap) {
         Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
 
-        RenderScript rs = RenderScript.create(activity);
+        RenderScript rs = RenderScript.create(activity.getApplicationContext());
         ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
         Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
         Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
@@ -123,6 +127,9 @@ public class PlayerUtils {
         theIntrinsic.setInput(tmpIn);
         theIntrinsic.forEach(tmpOut);
         tmpOut.copyTo(outputBitmap);
+        theIntrinsic.destroy();
+        rs.finish();
+        rs.destroy();
 
         return outputBitmap;
     }
