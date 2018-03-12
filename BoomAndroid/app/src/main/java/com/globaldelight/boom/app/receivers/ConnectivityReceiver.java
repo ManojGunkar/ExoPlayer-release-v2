aa2deddbe8c182f243d7e71295389be3d7ce8c9c
Adarsh <adarsh@globaldelight.com>
2018-03-12 20:52:58 +0530

    Connectivity Receiver fixes

--- BoomAndroid/app/src/main/java/com/globaldelight/boom/app/receivers/ConnectivityReceiver.java
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/app/receivers/ConnectivityReceiver.java
@@ -3,6 +3,7 @@ package com.globaldelight.boom.app.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
