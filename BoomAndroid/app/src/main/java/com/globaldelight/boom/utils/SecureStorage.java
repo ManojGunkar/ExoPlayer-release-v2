e1f60a86348efa91547172da92fa8d46cacd1d8d
Adarsh <adarsh@globaldelight.com>
2018-03-12 14:58:29 +0530

    Secure storage modifications from temp branch

--- BoomAndroid/app/src/main/java/com/globaldelight/boom/utils/SecureStorage.java
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/utils/SecureStorage.java
@@ -2,7 +2,6 @@ package com.globaldelight.boom.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.security.KeyPairGeneratorSpec;
 import android.security.keystore.KeyProperties;
 import android.util.Base64;
 
@@ -12,21 +11,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.math.BigInteger;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Random;
+import java.io.UnsupportedEncodingException;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
-import javax.security.auth.x500.X500Principal;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Created by adarsh on 11/12/17.
@@ -34,30 +26,29 @@ import javax.security.auth.x500.X500Principal;
 
 public class SecureStorage {
 
-    private final static String ALIAS = String.format("%s-%s", BuildConfig.APPLICATION_ID, "B2B");
-    private final static String TRANSFORMATION = "RSA/ECB/PKCS1Padding";
-    private final static String SIGN_ALGORITHM = "SHA256withRSA";
+    private final static String ALIAS = String.format("ss-%s-%s-0", BuildConfig.APPLICATION_ID, BuildConfig.FLAVOR);
+    private final static String ALIAS_ENCRYPTION_KEY = String.format("ss-%s-%s-1", BuildConfig.APPLICATION_ID, BuildConfig.FLAVOR);
+    private final static String TRANSFORMATION = KeyProperties.KEY_ALGORITHM_AES;
 
     private Context mContext;
     private String  mName;
-    private KeyStore mKeyStore;
+    private Certificate mCertificate;
 
     public SecureStorage(String name, Context context) {
         mContext = context;
         mName = name;
-        initKeys();
+        mCertificate = new Certificate(mContext, ALIAS);
+        initKey();
+    }
+
+    public static boolean exists(Context context, String name) {
+        File file = new File(getFilePath(context, name));
+        return file.exists();
     }
 
 
     public void store(byte[] data) {
         try {
-            KeyStore.PrivateKeyEntry privateKeyEntry =
-                    (KeyStore.PrivateKeyEntry) mKeyStore.getEntry(ALIAS, null);
-            PublicKey publicKey = privateKeyEntry.getCertificate().getPublicKey();
-
-            Cipher input = getCipher();
-            input.init(Cipher.ENCRYPT_MODE, publicKey);
-
             String path = getFilePath(mContext, mName);
             File file = new File(path);
             if ( !file.exists() ) {
@@ -65,7 +56,7 @@ public class SecureStorage {
             }
 
             FileOutputStream fileStream = new FileOutputStream(path);
-            CipherOutputStream encryptStream = new CipherOutputStream(fileStream, input);
+            CipherOutputStream encryptStream = new CipherOutputStream(fileStream, getCipher(Cipher.ENCRYPT_MODE));
 
             byte[] encodedBytes = Base64.encode(data, Base64.DEFAULT);
             encryptStream.write(encodedBytes);
@@ -80,16 +71,8 @@ public class SecureStorage {
 
     public byte[] load() {
         try {
-            KeyStore.PrivateKeyEntry privateKeyEntry =
-                    (KeyStore.PrivateKeyEntry) mKeyStore.getEntry(ALIAS, null);
-            PrivateKey privateKey = privateKeyEntry.getPrivateKey();
-
-            Cipher input = getCipher();
-            input.init(Cipher.DECRYPT_MODE, privateKey);
-
-
             FileInputStream fileStream = new FileInputStream(getFilePath(mContext, mName));
-            CipherInputStream decryptStream = new CipherInputStream(fileStream, input);
+            CipherInputStream decryptStream = new CipherInputStream(fileStream, getCipher(Cipher.DECRYPT_MODE));
 
             ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
             byte[] encodedBytes = new byte[4096];
@@ -99,10 +82,9 @@ public class SecureStorage {
 
                 byteStream.write(encodedBytes, 0, length);
             }
-
-            byte[] data = Base64.decode(byteStream.toByteArray(), Base64.DEFAULT);
             decryptStream.close();
 
+            byte[] data = Base64.decode(byteStream.toByteArray(), Base64.DEFAULT);
             return verifySign(data) ? data : null;
         }
         catch (Exception e) {
@@ -110,32 +92,6 @@ public class SecureStorage {
         }
     }
 
-    private void initKeys() {
-        try {
-            mKeyStore = KeyStore.getInstance("AndroidKeyStore");
-            mKeyStore.load(null);
-            if ( !mKeyStore.containsAlias(ALIAS) ) {
-                Calendar start = new GregorianCalendar();
-                Calendar end = new GregorianCalendar();
-                end.add(Calendar.YEAR, 10);
-
-                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(mContext)
-                        .setAlias(ALIAS)
-                        .setSubject(new X500Principal("CN=" + ALIAS))
-                        .setSerialNumber(new BigInteger(64, new Random(System.currentTimeMillis())))
-                        .setStartDate(start.getTime()).setEndDate(end.getTime())
-                        .build();
-
-                KeyPairGenerator kpg = KeyPairGenerator.getInstance(
-                        KeyProperties.KEY_ALGORITHM_RSA, mKeyStore.getProvider());
-                kpg.initialize(spec);
-                KeyPair kp = kpg.generateKeyPair();
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     private static String encryptedName(String name) {
         try {
@@ -143,32 +99,19 @@ public class SecureStorage {
             for ( int i = 0; i < bytes.length; i++ ) {
                 bytes[i] = (byte)(bytes[i] ^ 0xA1);
             }
-            return new String(Base64.encode(bytes, Base64.DEFAULT), "UTF-8");
+            return toBase64(bytes);
         }
         catch (Exception e) {
             return null;
         }
     }
 
-    private static Cipher getCipher() {
-        try {
-            return Cipher.getInstance(TRANSFORMATION);
-        } catch (Exception exception) {
-            throw new RuntimeException("Failed to get cipher", exception);
-        }
-    }
 
     private void sign(byte[] data) {
         try {
-            KeyStore.PrivateKeyEntry entry = (KeyStore.PrivateKeyEntry)mKeyStore.getEntry(ALIAS, null);
-            Signature s = Signature.getInstance(SIGN_ALGORITHM);
-            s.initSign(entry.getPrivateKey());
-            s.update(data);
-            byte[] signature = s.sign();
-
+            byte[] signature = mCertificate.sign(data);
             SharedPreferences pref = mContext.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
-            String signatureStr = new String(Base64.encode(signature, Base64.DEFAULT), "UTF-8");
-            pref.edit().putString(signatureKey(), signatureStr).apply();
+            pref.edit().putString(signatureKey(), toBase64(signature)).apply();
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -178,14 +121,8 @@ public class SecureStorage {
     private boolean verifySign(byte[] data) {
         try {
             SharedPreferences pref = mContext.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
-            byte[] signature = Base64.decode(pref.getString(signatureKey(), ""), Base64.DEFAULT);
-
-            KeyStore.PrivateKeyEntry entry = (KeyStore.PrivateKeyEntry)mKeyStore.getEntry(ALIAS, null);
-            Signature s = Signature.getInstance(SIGN_ALGORITHM);
-            s.initVerify(entry.getCertificate());
-            s.update(data);
-
-            return s.verify(signature);
+            byte[] signature =  dataFromBase64(pref.getString(signatureKey(), ""));
+            return mCertificate.verify(signature, data);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -198,11 +135,88 @@ public class SecureStorage {
     }
 
     private String signatureKey() {
-        return String.format("id-%s", mName);
+        return String.format("ss-id-%s", mName);
     }
 
-    public static boolean exists(Context context, String name) {
-        File file = new File(getFilePath(context, name));
-        return file.exists();
+
+    private void initKey() {
+        SharedPreferences pref = mContext.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+        String key = pref.getString(ALIAS_ENCRYPTION_KEY, null);
+        if ( key == null ) {
+            try {
+                KeyGenerator kg = KeyGenerator.getInstance(TRANSFORMATION);
+                kg.init(128);
+                SecretKey newKey = kg.generateKey();
+                byte[] encryptedKey = mCertificate.encrypt(newKey.getEncoded());
+                pref.edit().putString(ALIAS_ENCRYPTION_KEY, toBase64(encryptedKey)).apply();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+
+    private SecretKeySpec getEncryptionKey() {
+        try {
+            SharedPreferences pref = mContext.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+            String encodedKey = pref.getString(ALIAS_ENCRYPTION_KEY, null);
+            if ( encodedKey != null ) {
+                byte[] key = mCertificate.decrypt(dataFromBase64(encodedKey));
+                SecretKeySpec sp = new SecretKeySpec(key, TRANSFORMATION);
+                return sp;
+            }
+        }
+        catch (Exception e) {
+
+        }
+
+        return null;
+    }
+
+
+    private Cipher getCipher(int opmode) {
+        try {
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(opmode, getEncryptionKey());
+            return cipher;
+        } catch (Exception exception) {
+            throw new RuntimeException("Failed to get cipher", exception);
+        }
+    }
+
+    private static byte[] toBase64Data(String string) {
+        return toBase64(string).getBytes();
+    }
+
+    private static String toBase64(String string) {
+        return toBase64(string.getBytes());
+    }
+
+    private static String toBase64(byte[] data) {
+        try {
+            return new String(Base64.encode(data, Base64.DEFAULT), "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    private static byte[] dataFromBase64(String string) {
+        return fromBase64(string).getBytes();
+    }
+
+    private static String fromBase64(String string) {
+        return fromBase64(string.getBytes());
+    }
+
+    private static String fromBase64(byte[] data) {
+        try {
+            return new String(Base64.decode(data, Base64.DEFAULT), "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
 }
