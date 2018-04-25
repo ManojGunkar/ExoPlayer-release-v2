757a6bd9c9edbaaf120d03484c02281d85f45345
Manoj <manoj.k@globaldelight.com>
2018-04-25 18:32:32 +0530

    Created Tidal branch

---
+++ BoomAndroid/app/src/main/java/com/globaldelight/boom/tidal/WebConnector.java
@@ -0,0 +1,9 @@
+package com.globaldelight.boom.tidal;
+
+/**
+ * Created by Manoj Kumar on 25-04-2018.
+ * Copyright (C) 2018. Global Delight Technologies Pvt. Ltd. All rights reserved.
+ */
+public class WebConnector {
+    
+}
